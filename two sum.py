5cf748063fa81fe3b3523928adf7cf02192d737d
dh43a0198 <dh43a0198@gmail.com>
2021-06-09 12:47:08 +0900

    no message

---
+++ two sum.py
@@ -0,0 +1,17 @@
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#Example 1:
+#Input: nums = [2,7,11,15], target = 9
+#Output: [0,1]
+#Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+class Solution:
+   def twoSum(self, nums: List[int], target: int) -> List[int]:
+       seen = {}
+       for i, value in enumerate(nums): #1
+           remaining = target - nums[i] #2
+           
+           if remaining in seen: #3
+               return [i, seen[remaining]]  #4
+           else:
+               seen[value] = i  #5
+
